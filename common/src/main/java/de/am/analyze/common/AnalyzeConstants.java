7242a5e4b2c9e6c8d2c9b89db894a024cbd103aa
Martin Absmeier <martin.absmeier@gmx.de>
2022-12-18 17:33:32 +0100

    Add some constants

--- common/src/main/java/de/am/analyze/common/AnalyzeConstants.java
+++ common/src/main/java/de/am/analyze/common/AnalyzeConstants.java
@@ -15,10 +15,39 @@
  */
 package de.am.analyze.common;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 /**
  * {@code AnalyzeConstants} stores all constants of analyze application.
  *
  * @author Martin Absmeier
  */
 public class AnalyzeConstants {
+
+    // common ##########################################################################################################
+    public static final String EMPTY_STRING = "";
+    public static final String SEPARATOR = "--------------------------------------------------------------------------------";
+    public static final Charset DEFAULT_ENCODING = StandardCharsets.UTF_8;
+    public static final String USER_DIR = System.getProperty("user.dir");
+    public static final String USER_HOME_DIR = System.getProperty("user.home");
+    public static final String DEFAULT_DELIMITER = ".";
+    public static final String DEFAULT_DELIMITER_REGEX = "[.]";
+    public static final String DEFAULT_REVISION_ID = "revision id";
+
+    // parser ##########################################################################################################
+    public static final String JAVA_LANG_PACKAGE = "java.lang";
+    public static final String JAVA_IO_PACKAGE = "java.io";
+    public static final String JAVA_DEFAULT_PACKAGE_NAME = "default";
+    public static final String JAVA_MODIFIER_PUBLIC = "public";
+    public static final String JAVA_MODIFIER_PROTECTED = "protected";
+    public static final String JAVA_MODIFIER_STATIC = "static";
+    public static final String JAVA_MODIFIER_FINAL = "final";
+    public static final String JAVA_INSTANCE_INITIALIZER_NAME = "//instance_initializer";
+    public static final String JAVA_STATIC_INITIALIZER_NAME = "//static_initializer";
+    public static final String JAVA_ATT_KEY_METHOD_SIGNATURE = "method signature";
+    public static final String JAVA_ATT_KEY_CONTAINING_CLASS = "containing class";
+
+    // #################################################################################################################
+    protected AnalyzeConstants() { }
 }
